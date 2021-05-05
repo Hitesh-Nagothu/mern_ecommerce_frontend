d72557280fe1849573dbaa8bf44c461a73777bdd
Hitesh <hiteshnagothu@gmail.com>
2021-05-05 14:16:26 -0400

    Added addproduct component and API handler

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -2,11 +2,189 @@ import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isLoggedIn } from "../auth";
 import { Link } from "react-router-dom";
-import { createProduct } from "./apiAdmin";
+import { createProduct, getCategories } from "./apiAdmin";
 
 const AddProduct = () => {
+  const { user, token } = isLoggedIn();
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "",
+    categories: [],
+    category: "",
+    shipping: "",
+    quantity: "",
+    photo: "",
+    loading: false,
+    error: "",
+    createdProduct: "",
+    redirectToProfile: false,
+    formData: "",
+  });
 
-    const {user} = isLoggedIn();
+  const {
+    name,
+    description,
+    price,
+    categories,
+    category,
+    shipping,
+    quantity,
+    loading,
+    error,
+    createdProduct,
+    redirectToProfile,
+    formData,
+  } = values;
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, categories: data, formData: new FormData() });
+        console.log(categories);
+      }
+    });
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleChange = (objectName) => (event) => {
+    const value =
+      objectName === "photo" ? event.target.files[0] : event.target.value;
+
+    formData.set(objectName, value);
+
+    setValues({ ...values, [objectName]: value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+
+    setValues({ ...values, error: "", loading: true });
+
+    createProduct(user._id, token, formData).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          description: "",
+          photo: "",
+          price: "",
+          quantity: "",
+          loading: false,
+          createdProduct: data.name,
+        });
+      }
+    });
+  };
+
+  const newProductForm = () => {
+    return (
+      <form className="mb-3" onSubmit={clickSubmit}>
+        <h4>Product Photo</h4>
+        <div className="form-group">
+          <label className="btn btn-secondary">
+            <input
+              onChange={handleChange("photo")}
+              type="file"
+              name="photo"
+              accept="image/*"
+            />
+          </label>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            value={name}
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Description</label>
+          <textarea
+            onChange={handleChange("description")}
+            type="text"
+            className="form-control"
+            value={description}
+          ></textarea>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Price</label>
+          <input
+            onChange={handleChange("price")}
+            type="number"
+            className="form-control"
+            value={price}
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Category</label>
+          <select onChange={handleChange("category")} className="form-control">
+            <option>Select Category</option>
+
+            {categories &&
+              categories.map((c, idx) => {
+                return (
+                  <option key={idx} value={c._id}>
+                    {c.name}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Quantity</label>
+          <input
+            onChange={handleChange("quantity")}
+            type="number"
+            className="form-control"
+            value={quantity}
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Shipping</label>
+          <select onChange={handleChange("shipping")} className="form-control">
+            <option>Shipping Home?</option>
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+          </select>
+        </div>
+
+        <button className="btn btn-outline-primary">Create Product</button>
+      </form>
+    );
+  };
+
+  const showError = () => {
+      return (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+        {error}
+    </div>
+      );
+      
+  }
+
+  const showSuccess = () => {
+      return (
+        <div className="alert alert-info" style= {{dislayp: createdProduct ? '':'none'}}>
+        <h2>{`${createdProduct}`} is created</h2>
+    </div>
+      );
+     
+  }
 
   return (
     <Layout
@@ -15,11 +193,9 @@ const AddProduct = () => {
     >
       <div className="row">
         <div className="col-md-8 offset-md-2">
-          {/* {success && <h3 className="text-success">{name} is created</h3>}
-          {error && <h3 className="text-danger">Category should be unique</h3>} */}
-                ...
-          
-
+            {showError()}    
+            {showSuccess()}
+            {newProductForm()}
         </div>
       </div>
     </Layout>
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,39 +1,48 @@
 import { API } from "../config";
 
 export const createCategory = (userId, token, category) => {
-    //Making a POST REQUEST TO backend
-    return fetch(`${API}/category/create/${userId}`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "content-type": "application/json",
-        Authorization: `Bearer ${token}`
-      },
-      body: JSON.stringify(category),
+  //Making a POST REQUEST TO backend
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-
-  export const createProduct = (userId, token, product) => {
-    //Making a POST REQUEST TO backend
-    return fetch(`${API}/product/create/${userId}`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${token}`
-      },
-      body: product,
+export const createProduct = (userId, token, product) => {
+  //Making a POST REQUEST TO backend
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then((response) => {
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
